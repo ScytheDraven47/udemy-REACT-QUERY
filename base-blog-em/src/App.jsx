7daf8cd5a0955736e49f4499a026e8f603e90e53
Ben Rogers-McKee <ben.rogers.mckee@gmail.com>
2022-02-21 13:22:23 +1300

    refactor: initial format

--- base-blog-em/src/App.jsx
+++ base-blog-em/src/App.jsx
@@ -1,14 +1,14 @@
-import { Posts } from "./Posts";
-import "./App.css";
+import { Posts } from './Posts'
+import './App.css'
 
 function App() {
-  return (
-    // provide React Query client to App
-    <div className="App">
-      <h1>Blog Posts</h1>
-      <Posts />
-    </div>
-  );
+	return (
+		// provide React Query client to App
+		<div className='App'>
+			<h1>Blog Posts</h1>
+			<Posts />
+		</div>
+	)
 }
 
-export default App;
+export default App
