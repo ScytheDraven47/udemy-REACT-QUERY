d5852f53b43cd3a646f9e7c32d42966f0cdd7645
Ben Rogers-McKee <ben.rogers.mckee@gmail.com>
2022-02-23 08:33:30 +1300

    refactor: initial format

--- base-infinite-swapi/src/species/InfiniteSpecies.jsx
+++ base-infinite-swapi/src/species/InfiniteSpecies.jsx
@@ -1,13 +1,14 @@
-import InfiniteScroll from "react-infinite-scroller";
-import { Species } from "./Species";
+import InfiniteScroll from 'react-infinite-scroller'
+import { Species } from './Species'
 
-const initialUrl = "https://swapi.dev/api/species/";
+const initialUrl = 'https://swapi.dev/api/species/'
 const fetchUrl = async (url) => {
-  const response = await fetch(url);
-  return response.json();
-};
+	const response = await fetch(url)
+	return response.json()
+}
 
 export function InfiniteSpecies() {
-  // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+	// TODO: get data for InfiniteScroll via React Query
+
+	return <InfiniteScroll />
 }
