bc42f8e6da45580d01a6c556d6dc248c92942735
Ben Rogers-McKee <ben.rogers.mckee@gmail.com>
2022-02-22 15:27:07 +1300

    feat: implement pagination with prefetching

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,23 +1,39 @@
-import { useState } from 'react'
-import { useQuery } from 'react-query'
+import { useEffect, useState } from 'react'
+import { useQuery, useQueryClient } from 'react-query'
 
 import { PostDetail } from './PostDetail'
+
 const maxPostPage = 10
 
-async function fetchPosts() {
+async function fetchPosts(pageNumber) {
 	const response = await fetch(
-		'https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0'
+		`https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNumber}`
 	)
 	return response.json()
 }
 
 export function Posts() {
-	const [currentPage, setCurrentPage] = useState(0)
+	const [currentPage, setCurrentPage] = useState(1)
 	const [selectedPost, setSelectedPost] = useState(null)
 
-	const { data, isLoading, isError, error } = useQuery('posts', fetchPosts, {
-		staleTime: 2000,
-	})
+	const queryClient = useQueryClient()
+
+	useEffect(() => {
+		if (currentPage >= maxPostPage) return
+		const nextPage = currentPage + 1
+		queryClient.prefetchQuery(['posts', nextPage], () =>
+			fetchPosts(nextPage)
+		)
+	}, [currentPage, queryClient])
+
+	const { data, isLoading, isError, error } = useQuery(
+		['posts', currentPage],
+		() => fetchPosts(currentPage),
+		{
+			staleTime: 2000,
+			keepPreviousData: true,
+		}
+	)
 	if (isLoading) return <h3>Loading...</h3>
 	if (isError)
 		return (
@@ -41,11 +57,17 @@ export function Posts() {
 				))}
 			</ul>
 			<div className='pages'>
-				<button disabled onClick={() => {}}>
+				<button
+					disabled={currentPage <= 1}
+					onClick={() => setCurrentPage((prev) => prev - 1)}
+				>
 					Previous page
 				</button>
-				<span>Page {currentPage + 1}</span>
-				<button disabled onClick={() => {}}>
+				<span>Page {currentPage}</span>
+				<button
+					disabled={currentPage >= maxPostPage}
+					onClick={() => setCurrentPage((prev) => prev + 1)}
+				>
 					Next page
 				</button>
 			</div>
