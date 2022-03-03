bd0dc30b3181cdb30e61f20a3a71af89f6c165f9
Ben Rogers-McKee <ben.rogers.mckee@gmail.com>
2022-03-04 11:43:11 +1300

    feat: refetch appointments on interval

--- base-lazy-days/client/src/components/appointments/hooks/useAppointments.ts
+++ base-lazy-days/client/src/components/appointments/hooks/useAppointments.ts
@@ -106,6 +106,7 @@ export function useAppointments(): UseAppointments {
       refetchOnMount: true,
       refetchOnReconnect: true,
       refetchOnWindowFocus: true,
+      refetchInterval: 60000, // 1 minute
     },
   );
 
